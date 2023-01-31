bbd03bde7a69d805159079343f00ef0ab9f969e2
Mike Barkmin <mike@barkmin.eu>
2023-01-31 02:17:56 +0100

    fix wrong references

--- examples/reference/SpriteGoLayersBackwards.java
+++ examples/reference/SpriteGoLayersBackwards.java
@@ -25,7 +25,7 @@ public class SpriteGoLayersBackwards {
     }
 
     public static void main(String[] args) {
-        new SpriteGoLayersForwards();
+        new SpriteGoLayersBackwards();
     }
 }
 
