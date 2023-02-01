c02a8dcd016a00f9a2af8ae13fda1c6824140685
Mike Barkmin <mike@barkmin.eu>
2023-02-01 08:47:17 +0100

    add better intro

---
+++ docs/en/archives/Halloween/HalloweenStage.java
@@ -0,0 +1,27 @@
+import org.openpatch.scratch.*;
+
+public class HalloweenStage extends Stage
+{    
+    int numberPumpkins = 10;
+    HouseSprite house;
+    GhostSprite ghost;
+    
+    public HalloweenStage() {
+        this.addBackdrop("bg", "sprites/background.jpg");
+        this.addSound("bg", "sounds/background.wav");
+        
+        house = new HouseSprite();
+        this.add(house);
+    
+        for (int i = 0; i < numberPumpkins; i++) {
+            PumpkinSprite p = new PumpkinSprite();
+            this.add(p);
+        }
+        ghost = new GhostSprite();
+        this.add(ghost);
+    }
+    
+    public void run() {
+        this.playSound("bg");
+    }
+}
