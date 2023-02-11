e68ce9296e19112ce523804decb08a5272149d45
Mike Barkmin <mike@barkmin.eu>
2023-02-11 15:41:19 +0100

    fix build script

---
+++ docs/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd ..
+
+shopt -s expand_aliases
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+. $DIR/.env
+
+cp ./CHANGELOG.md ./docs/en/book/changelog.md
+s4j_examples_copy_to_documentation
+
+popd
+
+npx hyperbook build
