afe61e6a0694b2dc2a734afe8cda78d48a180668
Mike Barkmin <mike@barkmin.eu>
2023-04-14 23:56:35 +0200

    add smart rocket example

---
+++ examples/java/SmartRocket/SmartRocket.java
@@ -0,0 +1,12 @@
+import org.openpatch.scratch.*;
+
+public class SmartRocket extends Window {
+    public SmartRocket() {
+        super(800, 600, "assets");
+        this.addStage("level", new Level());
+    }
+
+    public static void main(String[] args) {
+        new SmartRocket();
+    }
+}
