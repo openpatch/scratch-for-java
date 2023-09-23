33ef5bed9ec585a75912b68e4fa3f6f9d1827cc3
Mike Barkmin <mike@barkmin.eu>
2023-09-23 10:32:19 +0200

    fix docs and examples not using setStage

--- examples/java/SmartRocket/SmartRocket.java
+++ examples/java/SmartRocket/SmartRocket.java
@@ -3,7 +3,7 @@ import org.openpatch.scratch.*;
 public class SmartRocket extends Window {
   public SmartRocket() {
     super(800, 600, "assets");
-    this.addStage("level", new Level());
+    this.setStage(new Level());
   }
 
   public static void main(String[] args) {
