b1b6ace5dbb0bf3942c457c4a7bdeb42ecc5d37c
mikebarkmin <mike@barkmin.eu>
2025-11-30 10:58:29 +0100

    update description

--- src/main/java/org/openpatch/scratch/extensions/package-info.java
+++ src/main/java/org/openpatch/scratch/extensions/package-info.java
@@ -1,4 +1,16 @@
 /**
+ * This package contains extensions for Scratch for Java.
+ * 
+ * For example, it includes extensions for animations:
+ * 
+ * <pre>{@code
+ * import org.openpatch.scratch.extensions.animation.AnimatedSprite;
+ * 
+ * AnimatedSprite sprite = new AnimatedSprite();
+ * sprite.addAnimation("walk", "assets/walk_%d.png", 4);
+ * sprite.playAnimation("walk");
+ * }</pre>
+ * 
  * @name-in-docs Extensions
  * @index-in-docs 4
  */
